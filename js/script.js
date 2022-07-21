516de40d5698633712de43427ebd6855ffcde695
ElmoWoo <hello@xuedesign.tw>
2022-07-21 16:44:49 +0800

    頁面製作 #2022-07-21
    - PUG架構整理
    - 首頁製作完成
    - 搜尋頁拷貝首頁，作些微調整

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-loadjs("https://cdn.jsdelivr.net/npm/lozad@1.16.0/dist/lozad.min.js",(function(){lozad().observe()})),loadjs(["https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css","https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"]);
+//lazyload
+loadjs('https://cdn.jsdelivr.net/npm/lozad@1.16.0/dist/lozad.min.js', function() {
+    const observer = lozad();
+    observer.observe();
+});
+
+//fancybox
+loadjs(['https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css', 'https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js']);
+
+//headerroll
+loadjs('js/headerroll.js');
