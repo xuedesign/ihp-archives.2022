1dade3fdfae22f045302c0854ae27a08f783fc62
ElmoWoo <hello@xuedesign.tw>
2022-07-22 17:55:14 +0800

    整編原則頁面製作 #2022-07-22

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ loadjs('https://cdn.jsdelivr.net/npm/lozad@1.16.0/dist/lozad.min.js', function()
     observer.observe();
 });
 
-//fancybox
-loadjs(['https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css', 'https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js']);
-
 //headerroll
 loadjs('js/headerroll.js');
+
+//fancybox
+loadjs(['https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css', 'https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js']);
