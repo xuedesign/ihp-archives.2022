34653696e070177f27144fa9a274e29e7b21be17
ElmoWoo <hello@xuedesign.tw>
2022-08-11 17:56:08 +0800

    簡目清單頁面製作 #2022-08-11
    - 新增頁碼控制

--- js/script.js
+++ js/script.js
@@ -7,5 +7,8 @@ loadjs('https://cdn.jsdelivr.net/npm/lozad@1.16.0/dist/lozad.min.js', function()
 //headerroll
 loadjs('https://xuedesign.github.io/ihp-archives.2022/js/headerroll.js');
 
+//checkall
+loadjs('https://xuedesign.github.io/ihp-archives.2022/js/checkall.js');
+
 //fancybox
 loadjs(['https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css', 'https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js']);
