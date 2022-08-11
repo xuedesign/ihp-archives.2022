f90762bc7002ecfcee5384b02ebee6024ded38d4
ElmoWoo <hello@xuedesign.tw>
2022-08-11 18:17:49 +0800

    修改checkbox全選功能 #2022-08-11

--- js/checkall.js
+++ js/checkall.js
@@ -1,5 +1,26 @@
-function check_all(obj,cName)
-    {
-        var checkboxs = document.getElementsByName(cName);
-        for(var i=0;i<checkboxs.length;i++){checkboxs[i].checked = obj.checked;}
+document.getElementById("mytable").addEventListener("change",(e)=>{
+    if(!e.target.checked){
+      document.querySelector("input[type=checkbox][name=selectItem]").checked=false;
+    }else{
+      const checkBoxCount = document.querySelectorAll("input[type=checkbox]:not([name=selectItem])").length;
+      
+      const selectedCheckBoxCount = document.querySelectorAll("input[type=checkbox]:checked:not([name=selectItem])").length;
+      console.log(selectedCheckBoxCount)
+      if(checkBoxCount===selectedCheckBoxCount){
+        document.querySelector("input[type=checkbox][name=selectItem]").checked=true;
+      }
     }
+ })
+ 
+ document.querySelector("input[name=selectItem]").addEventListener("change",
+ (e)=>{
+     const allCheckBoxElements = document.querySelectorAll("input[type=checkbox]:not([name=selectItem])");
+     allCheckBoxElements.forEach(el =>{
+       
+       if(e.target.checked){
+         el.checked = true;
+       }else {
+         el.checked = false;
+       }
+     })
+ });
