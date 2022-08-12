45b160aac0ff670f42067c8a514da7b81c2c6393
ElmoWoo <hello@xuedesign.tw>
2022-08-12 18:27:54 +0800

    頁面修改 #2022-08-12
    - 使用注意警語修改
    - 表格全選功能修正

--- js/checkall.js
+++ js/checkall.js
@@ -1,4 +1,44 @@
-function check_all(obj,cName){
-	var checkboxs = document.getElementsByName(cName);
-	for(var i=0;i<checkboxs.length;i++){checkboxs[i].checked = obj.checked;}
+var checkbox = {
+  init: function(){
+    var that = this;
+    
+    $( '.mytable' ).each( function(){
+        $parent = $( this );
+
+        that.activeChildsByParent( $parent );
+        
+        $parent.find( 'input[type="checkbox"]:not(".all")' ).on( 'change', function(){
+          var $thisParent = $( this ).closest( '.mytable' );
+          that.activeChildsByParent( $thisParent );
+        });
+
+        $parent.find( 'input[type="checkbox"].all' ).on( 'change', function(){
+          var $thisParent = $( this ).closest( '.mytable' );
+          that.toggleAllChildsByParent( $thisParent );
+        })
+
+      });
+  },
+  toggleAllChildsByParent:function( $parent ){
+    var $childs = $parent.find( 'input[type="checkbox"]:not(.all)' ),
+        stateAll = $parent.find( 'input[type="checkbox"].all' ).prop( 'checked' );
+
+    $childs.prop( 'checked', stateAll );
+    this.activeChildsByParent( $parent );
+  },
+  activeChildsByParent:function( $parent ){
+    var $allChilds = $parent.find( 'input[type="checkbox"].all' ),
+        total = $parent.find( 'input[type="checkbox"]:not(.all)' ).length,
+        actives = $parent.find( 'input[type="checkbox"]:not(.all):checked').length;
+
+    if( actives < total){
+      $allChilds.prop( 'checked', false );
+    }else{
+      $allChilds.prop( 'checked', true );
+    }
+  }
 }
+
+$( document ).ready( function(){
+checkbox.init();
+})
