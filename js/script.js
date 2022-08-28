c83b7c9c9f2f150f57e34c7b29005c66a05d7ac1
ElmoWoo <hello@xuedesign.tw>
2022-08-28 18:29:49 +0800

    檔案詳目新增圖片輪播 #2022-08-28

--- js/script.js
+++ js/script.js
@@ -20,5 +20,24 @@ loadjs(['https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css', 'htt
 loadjs('https://xuedesign.github.io/ihp-archives.2022/js/dialog.js');
 //loadjs('../js/dialog.js');
 
+//slide-img
+loadjs('https://cdn.jsdelivr.net/npm/flexslider/jquery.flexslider.min.js', function() {
+    $(window).load(function() {
+        $('.flexslider').flexslider({
+          animation: "slide"
+        });
+      });
+});
+
+//lightgallery
+loadjs('https://cdn.jsdelivr.net/npm/lightgallery/lightgallery.umd.min.js',function() {
+    lightGallery(document.getElementById('lightgallery'),{
+        //plugins: [lgZoom, lgThumbnail],
+        licenseKey: '0000-0000-000-0000',
+        speed: 500,
+        // ... other settings
+    });
+});
+
 //print-img
 loadjs('https://cdn.jsdelivr.net/npm/print-js@1.6.0/dist/print.min.js');
