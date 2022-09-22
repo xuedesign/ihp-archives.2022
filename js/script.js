3b13edc474d4fe276e36a7c87111874a406fdbef
ElmoWoo <hello@xuedesign.tw>
2022-09-22 18:05:55 +0800

    會員頁面製作 #2022-09-22
    - 輸入框動態效果
    - 忘記密碼頁
    - 重設密碼頁

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ loadjs('https://cdn.jsdelivr.net/npm/lozad@1.16.0/dist/lozad.min.js', function()
 });
 
 //headerroll
-loadjs('https://xuedesign.github.io/ihp-archives.2022/js/headerroll.js');
+loadjs('https://ihparchives.pages.dev/js/headerroll.js');
 
 //checkall
-loadjs('https://xuedesign.github.io/ihp-archives.2022/js/checkall.js');
+loadjs('https://ihparchives.pages.dev/js/checkall.js');
 
 //fontawesome
 loadjs('https://kit.fontawesome.com/c66b525810.js');
@@ -17,7 +17,7 @@ loadjs('https://kit.fontawesome.com/c66b525810.js');
 loadjs(['https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.css', 'https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js']);
 
 //dialog
-loadjs('https://xuedesign.github.io/ihp-archives.2022/js/dialog.js');
+loadjs('https://ihparchives.pages.dev/js/dialog.js');
 //loadjs('../js/dialog.js');
 
 //slide-img
@@ -30,7 +30,7 @@ loadjs(['https://ihparchives.pages.dev/css/flexslider.css','https://cdn.jsdelivr
 });
 
 //login
-loadjs('https://xuedesign.github.io/ihp-archives.2022/js/login.js');
+loadjs('https://ihparchives.pages.dev/js/login.js');
 
 //lightgallery
 loadjs(['https://ihparchives.pages.dev/css/viewer.css','https://cdn.jsdelivr.net/npm/viewerjs@1.10.5/dist/viewer.min.js'],function() {
